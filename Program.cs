60307956c64396d62f48d1fa78e362b6b2cbcdcf
Mayara Pinheiro <mayarapinheirodesousa@hotmail.com>
2022-08-05 15:40:29 -0300

    Exercicio

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace CalcularSalario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            int numero = int.Parse(Console.ReadLine());
+            int horas = int.Parse(Console.ReadLine());
+            double valorHora = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double salario = valorHora * horas;
+
+            Console.WriteLine( $"NUMBER = {numero} ");
+            Console.WriteLine($"SALARY = U$ {salario.ToString("F2", CultureInfo.InvariantCulture)}");
+
+            Console.ReadKey();
+        }
+    }
+}
